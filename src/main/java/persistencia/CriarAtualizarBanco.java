e7a680639801dd913dcb4ff959ba0bf615367600
renan2014IFRN <renan.lima@academico.ifrn.edu.br>
2017-07-06 16:54:45 -0300

    implementando realizar login(*Classe CriarAtualizaBanco)

---
+++ src/main/java/persistencia/CriarAtualizarBanco.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+/**
+ *
+ * @author renan
+ */
+public class CriarAtualizarBanco {
+    
+    private CriarAtualizarBanco() {
+        
+    }
+    
+    public static void main(String[] args) {
+        JPAUtil.fecharEntityManager(JPAUtil.getEntityManager());
+        JPAUtil.fecharEntityManageFactory();
+    }
+    
+}
